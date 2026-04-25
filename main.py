5cb855929736df35463ebb48ba5df694fb64e147
Ahmed Emara <ahmed6emara@gmail.com>
2026-04-26 02:48:00 +0300

    better code and xgb stuff

--- main.py
+++ main.py
@@ -19,69 +19,87 @@ from xgboost import XGBRegressor
 from catboost import CatBoostRegressor
 import lightgbm as lgb
 from sklearn.preprocessing import LabelEncoder
-SEED = 1234
+SEED = 442004
 
 
 
 #%% main functions
 
-def preprocess(df: pd.DataFrame) -> pd.DataFrame:
-    for index,row in df.iterrows():
-        if row['Outlet_Location_Tier'] == 'Tier 2' and row['Outlet_Type'] == 'Supermarket Type1':
-            df.at[index,'Outlet_Size'] = 'Small'
-        elif row['Outlet_Location_Tier'] == 'Tier 3' and row['Outlet_Type'] == 'Grocery Store':
-            df.at[index,'Outlet_Size'] = 'Medium'
+def preprocess(df: pd.DataFrame, remove_nulls:bool) -> pd.DataFrame:
+    if remove_nulls:
+        for index,row in df.iterrows():
+            if row['Outlet_Location_Tier'] == 'Tier 2' and row['Outlet_Type'] == 'Supermarket Type1':
+                df.at[index,'Outlet_Size'] = 'Small'
+            elif row['Outlet_Location_Tier'] == 'Tier 3' and row['Outlet_Type'] == 'Grocery Store':
+                df.at[index,'Outlet_Size'] = 'Medium'
 
-    # df['Item_Weight'] = df['Item_Weight'].fillna(df['Item_Weight'].median())
-    df = df.drop(columns=['Item_Weight'])
+        # df['Item_Weight'] = df['Item_Weight'].fillna(df['Item_Weight'].median())
+        df = df.drop(columns=['Item_Weight'])
 
     df = df.drop(columns=['Item_Identifier'])
     df = pd.get_dummies(df, drop_first=True)
     return df
 
-def get_train():
+def get_train(do_preprocess = True, remove_nulls = True):
     df = pd.read_csv("train.csv")
-    df = preprocess(df)
+    if do_preprocess:
+        df = preprocess(df, remove_nulls=remove_nulls)
     columns = set(df.columns)
     X = df[sorted(list(columns - {'Y'}))]
     y = df['Y']
     return X, y
 
-def get_test():
+def get_test(remove_nulls=True):
     df = pd.read_csv("test.csv")
-    df = preprocess(df)
+    df = preprocess(df,remove_nulls)
     return df[sorted(df.columns)]
 
-def do_test(model):
-    prediction = model.predict(get_test())
+def do_test(model, remove_nulls = True):
+    prediction = model.predict(get_test(remove_nulls))
     submission = pd.DataFrame({
-        'row_id': range(1, len(prediction) + 1),
-        'prediction': prediction
+        'row_id': range(0, len(prediction)),
+        'Y': prediction
     })
     submission.to_csv('output.csv',index=False)
 
+def print_errors(model):
+    predTest = model.predict(X_test)
+    mae = mean_absolute_error(y_test, predTest)
+    print(f"mean absolute error = {float(mae)}")
 
 #%%
 
-X,y = get_train()
+X,y = get_train(remove_nulls=False)
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size = 0.2, # ratio
     random_state = SEED)
 
+#%%
 len_reg = Pipeline([
     ("model", LinearRegression())
     ]) 
 len_reg.fit(X_train, y_train)
-len_predTest = len_reg.predict(X_test)
-len_predTrain = len_reg.predict(X_train) 
-mae = mean_absolute_error(y_train, len_predTrain)
-mape = np.mean(np.abs((y_test - len_predTest) / y_test)) * 100
-print(mae)
-print(mape)
+
+print_errors(len_reg)
+do_test(len_reg)
 
 #%%
-do_test(len_reg)
+XG_reg = XGBRegressor(
+        n_estimators=1000,
+        learning_rate=0.05,
+        max_depth=4,
+        early_stopping_rounds=20,
+        eval_metric = 'rmse',
+        subsample=0.8,
+        random_state=SEED
+        )
+XG_reg.fit(X_train,y_train,
+           eval_set=[(X_test,y_test)],
+           verbose=False)
+
+print_errors(XG_reg)
+do_test(XG_reg,remove_nulls=False)
 
 # commented code that is too valuable to remove
 # #%% guess imputation thing
